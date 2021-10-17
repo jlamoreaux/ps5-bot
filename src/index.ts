26110ad14ed44ae1dc67fbdc293e93966c577cec
Jordan <jnacious88@gmail.com>
2021-10-16 23:58:09 -0500

    just testing :|

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,8 @@ const app = () => {
   const itemsToQuery: Array<item> = [
     {
       name: 'PS5 - Digital',
-      url: 'https://www.target.com/p/playstation-5-digital-edition-console/-/A-81114596',
+      // url: 'https://www.target.com/p/playstation-5-digital-edition-console/-/A-81114596',
+      url: 'https://jlamoreaux.com',
       isInStock: false,
       lastNotification: Date.now(),
     },
